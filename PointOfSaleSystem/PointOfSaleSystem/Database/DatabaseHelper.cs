5ded92f772914b3189b35867c320786a1aa0dc73
ivar117 <ivar.bjerling@elev.ga.ntig.se>
2024-11-29 14:49:27 +0100

    added support for comments in .txt file
    Added functionality to InsertProductsData function in DatabaseHelper.cs to support comments in the .txt file containing initial products data.  Additionally reworked the structure of the InsertProductsData function.

--- PointOfSaleSystem/PointOfSaleSystem/Database/DatabaseHelper.cs
+++ PointOfSaleSystem/PointOfSaleSystem/Database/DatabaseHelper.cs
@@ -109,50 +109,66 @@ namespace PointOfSaleSystem.Database
                 return false;
             }
 
-            int current_category = int.Parse(fileLines[0]);
+            int currentCategoryId = 1; // Used to track which category each product is assigned to
             string currentLine;
             string[] lineSplit;
             bool isspace;
             int insertCount = 0; // Track the number of successful inserts
 
-            // Start loop on the first item in the initial category
-            for (int i = 1; i < fileLines.Length; i++)
+            // Loop through each line from the .txt file and add the entries to the "products" table
+            for (int i = 0; i < fileLines.Length; i++)
             {
                 currentLine = fileLines[i];
 
                 isspace = currentLine.All(char.IsWhiteSpace);
 
-                // Continue with the next category of products when a blank line is found
-                if (isspace)
-                {
-                    // Next line after a blank line will be the next category ID
-                    i++;
-                    current_category = int.Parse(fileLines[i]);
-                    continue;
-                }
-
-                lineSplit = currentLine.Split(',');
-                string productName = lineSplit[0];
-                string productAutomationId = CreateNameId(productName); // Generate unique product name ID for UI automation
-                int productPrice = int.Parse(lineSplit[1]);
-
-                try
-                {
-                    sqlite_cmd.CommandText = "INSERT INTO products (product_name, product_automation_id, price, category_id) VALUES (@product_name, @product_automation_id, @price, @category_id);";
-
-                    sqlite_cmd.Parameters.Clear(); // Clear parameters before adding new ones
-                    sqlite_cmd.Parameters.AddWithValue("@product_name", productName);
-                    sqlite_cmd.Parameters.AddWithValue("@product_automation_id", productAutomationId);
-                    sqlite_cmd.Parameters.AddWithValue("@price", productPrice);
-                    sqlite_cmd.Parameters.AddWithValue("@category_id", current_category);
-
-                    sqlite_cmd.ExecuteNonQuery();
-                }
-                catch (Exception ex)
+                // Treat lines starting with '#' as comments
+                if (currentLine.StartsWith('#'))
                 {
                     continue;
                 }
-                insertCount++; // Increment successful insert count
+                // Skip if blank line is found
+                else if (isspace)
+                {
+                    continue;
+                }
+                // Redefine currentCategoryId if the line only contains a number (indicating new category id) and then move to the next line
+                else if (int.TryParse(currentLine.Trim(), out int categoryId))
+                {
+                    currentCategoryId = categoryId;
+                    continue;
+                }
+                else
+                {
+                    lineSplit = currentLine.Split(',');
+
+                    // Skip if not enough data
+                    if (lineSplit.Length < 2)
+                    {
+                        continue;
+                    }
+                    string productName = lineSplit[0];
+                    string productAutomationId = CreateNameId(productName); // Generate unique product name ID for UI automation
+                    int productPrice = int.Parse(lineSplit[1]);
+
+                    try
+                    {
+                        sqlite_cmd.CommandText = "INSERT INTO products (product_name, product_automation_id, price, category_id) VALUES (@product_name, @product_automation_id, @price, @category_id);";
+
+                        sqlite_cmd.Parameters.Clear(); // Clear parameters before adding new ones
+                        sqlite_cmd.Parameters.AddWithValue("@product_name", productName);
+                        sqlite_cmd.Parameters.AddWithValue("@product_automation_id", productAutomationId);
+                        sqlite_cmd.Parameters.AddWithValue("@price", productPrice);
+                        sqlite_cmd.Parameters.AddWithValue("@category_id", currentCategoryId);
+
+                        sqlite_cmd.ExecuteNonQuery();
+                    }
+                    catch (Exception ex)
+                    {
+                        continue;
+                    }
+                    insertCount++; // Increment successful insert count
+                }
             }
 
             if (insertCount > 0)
